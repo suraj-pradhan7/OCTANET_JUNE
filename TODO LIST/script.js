7bc058270f034ecb68c6b84d6456c6c371db68a6
suraj-pradhan7 <surajpradhan713522@gmail.com>
2024-06-16 12:54:24 +0530

    Remove TODO LIST folder

---
+++ TODO LIST/script.js
@@ -0,0 +1,34 @@
+const inputbox=document.getElementById("input-text");
+const listcontainer=document.getElementById("list-container");
+function addTask()
+{
+    if(inputbox.value=='')
+        {
+            alert("Please Add Task");
+        }
+        else{
+            let li=document.createElement("li");
+            li.innerHTML=inputbox.value;
+            listcontainer.appendChild(li);
+            inputbox.value='';
+            let span=document.createElement("span");
+            span.innerHTML="\u00d7";
+            li.appendChild(span);
+        }
+        inputbox.value="";
+        savedata();
+}
+listcontainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");savedata();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();savedata();
+    }
+}, false);
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showTask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
